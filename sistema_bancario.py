7ba2aef616217dfeb33154f88c141386c315365c
Carlos Eduardo Fernandes <dufernandess@hotmail.com>
2024-09-26 12:31:24 -0300

    Criando funções específicas e novas funcionalidades ao projeto

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,69 +1,130 @@
-menu = """
+def menu():
+    menu = """
+            Seja bem-vindo!
+    Por favor, digite a opção desejada
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [cd] Cadastrar Cliente
+    [lc] Listar Contas
+    [q] Sair
+    =>"""
+    return input(menu)
 
-        Seja bem-vindo!
-Por favor, digite a opção desejada
-
-        [d] Depositar
-        [s] Sacar
-        [e] Extrato
-        [q] Sair
-
-=>"""
-
-saldo = 0
-limite = 500
-extrato = []
-numero_saques = 0
-LIMITE_SAQUES = 3
-
-
-while True:
-    opcao = input(menu)
-
-    if opcao == "d":
-        valor_deposito = float(input("Digite o valor que deseja depositar: "))
-
-        if valor_deposito > 0:
-            saldo += valor_deposito
-            extrato_deposito = f"Depósito: R$ {valor_deposito:.2f}"
-            extrato.append(extrato_deposito)
-
-        else:
-            print("Erro! O valor é inválido!")
-
-    elif opcao == "s":
-        valor_saque = float(input("Digite o valor que deseja sacar: "))
-
-        if valor_saque <= 500:
-
-            if valor_saque > saldo:
-                print("Não há saldo disponível para o saque!")
-
-            elif valor_saque < saldo and valor_saque > limite:
-                print("O limite de saque é de até R$500.00")
-
-            elif valor_saque < saldo and valor_saque < limite and numero_saques >= LIMITE_SAQUES:
-                print("Você já excedeu o limite de saques diários!")
-
-            elif valor_saque < saldo and valor_saque < limite and numero_saques < LIMITE_SAQUES:
-                saldo -= valor_saque
-                numero_saques += 1
-                extrato_saque = f"Saque: R$ {valor_saque:.2f}"
-                extrato.append(extrato_saque)
-
-        else:
-            print("Erro: O valor é inválido")
-
-    elif opcao == "e":
-        print("==========EXTRATO==========\n")
-        for item in extrato:
-            print(item)
-        print(f"\nSaldo: R$ {saldo:.2f}\n")
-        print("========================\n")
-        print("Agrademos a sua preferência! Até mais!")
-
-    elif opcao == "q":
-        break
+def deposito(valor_deposito, saldo, extrato):
+    
+    if valor_deposito > 0:
+        saldo += valor_deposito
+        extrato.append({"Deposito": valor_deposito})
 
     else:
-        print("Opção inválida, por favor, tente novamente")
+        print("Erro! O valor é inválido!")
+
+    return saldo, extrato
+
+def saque(valor_saque, saldo, extrato, limite_saque, NUMERO_SAQUES, contagem_saque):
+
+    excedeu_saldo = valor_saque > saldo
+    excedeu_limite = valor_saque > limite_saque
+    excedeu_saques = contagem_saque >= NUMERO_SAQUES
+
+    if excedeu_saldo:
+        print("\nOperação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("\nOperação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("\nOperação falhou! Número máximo de saques excedido.")
+
+    elif valor_saque > 0:
+        saldo -= valor_saque
+        contagem_saque += 1
+        extrato.append({"Saque": valor_saque})
+
+    else:
+        print("Erro: O valor é inválido")
+
+    return saldo, extrato, limite_saque, NUMERO_SAQUES, contagem_saque
+
+def exibir_extrato(saldo, extrato):
+    print("==========EXTRATO==========\n")
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+    for transacao in extrato:
+        for tipo, valor in transacao.items():
+            print(f"{tipo}: R$ {valor:.2f}")
+    print(f"\nSaldo: R$ {saldo:.2f}\n")
+    print("========================\n")
+    print("Agrademos a sua preferência! Até mais!")
+    
+def cadastrar_cliente(AGENCIA, num_conta, clientes, conta_bancaria):
+    cpf = input("Digite o cpf do usuário (somente números): ")
+    
+    usuario = confirmar_cpf(cpf, clientes)
+
+    if(usuario):
+        print("Usuário já cadastrado!")
+        return
+
+    nome = input("Digite o nome completo do usuário: ")
+    data_nascimento = input("Digite a data de nascimento do usuário no formato (AA/MM/DD): ")    
+    endereco = input("Digite o endereco do usuário no formato (logradouro, nro, bairro, cidade/sigla estado): ")
+
+    cliente = {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco}
+    clientes.append(cliente)
+
+    conta_bancaria.append({"Agencia": AGENCIA, "Número da conta": num_conta, "Cliente": cliente})
+
+    print("Usuário cadastrado com sucesso!")
+    
+def confirmar_cpf(cpf, clientes):
+    for usuario in clientes:
+        if usuario["cpf"] == cpf:
+            return usuario
+        return None
+
+def listar_contas(conta_bancaria):
+    for conta in conta_bancaria:
+                print("Agência:", conta["Agencia"],"Nro Conta:", conta["Número da conta"],"Cliente:", conta["Cliente"]["nome"], "CPF:", conta["Cliente"]["cpf"])
+
+def operacoes_menu():
+    
+    saldo = 0
+    num_conta = 1
+    limite_saque = 500
+    extrato = []
+    clientes = []
+    conta_bancaria = []
+    contagem_saque = 0
+    AGENCIA = "0001"
+    NUMERO_SAQUES = 3
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor_deposito = float(input("Digite o valor que deseja depositar: "))
+            saldo, extrato = deposito(valor_deposito, saldo, extrato)
+
+        elif opcao == "s":
+            valor_saque = float(input("Digite o valor que deseja sacar: "))
+            saldo, extrato, limite_saque, NUMERO_SAQUES, contagem_saque = saque(valor_saque, saldo, extrato, limite_saque, NUMERO_SAQUES, contagem_saque)
+            
+        elif opcao == "e":
+           exibir_extrato(saldo, extrato)
+        
+        elif opcao == "cd":
+            cadastrar_cliente(AGENCIA, num_conta, clientes, conta_bancaria)
+            num_conta += 1
+
+        elif opcao == "lc":
+            listar_contas(conta_bancaria)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Opção inválida, por favor, tente novamente")
+
+operacoes_menu()
